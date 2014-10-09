864971b9dcec6e370a019e4c8c9713017a925ea2
gerswin <gerswin@parawebs.com>
2014-10-09 14:51:35 -0430

    fix

--- index.php
+++ index.php
@@ -1,9 +1,144 @@
 <?php
 require 'vendor/autoload.php';
-
+use Mailgun\Mailgun;
 
 $app = new \Slim\Slim();
-$app->database = new medoo();
+$app->database = new medoo([
+
+    'database_type' => 'mysql',
+    'database_name' => 'expofiss',
+    'server' => '127.0.0.1',
+    'username' => 'root',
+    'password' => 'root',
+    'port' => 3306,
+    'charset' => 'utf8',
+    'option' => [
+        //PDO::ATTR_CASE => PDO::CASE_NATURAL,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ]
+    ]);
+
+$app->mg = new Mailgun("key-76u-fd2ilxwmtid-dm9rgq69dikp6km3");
+$app->mg_domain = "tuquiniela.net";
+$app->email = <<<EOF
+   <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; margin: 0; padding: 0;">
+<head>
+<meta name="viewport" content="width=device-width" />
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title> Preventa - Expoferia 2015 </title>
+
+
+<style type="text/css">
+img {
+max-width: 100%;
+}
+body {
+-webkit-font-smoothing: antialiased; -webkit-text-size-adjust: none; width: 100% !important; height: 100%; line-height: 1.6;
+}
+body {
+background-color: #f6f6f6;
+}
+@media only screen and (max-width: 640px) {
+  h1 {
+    font-weight: 600 !important; margin: 20px 0 5px !important;
+  }
+  h2 {
+    font-weight: 600 !important; margin: 20px 0 5px !important;
+  }
+  h3 {
+    font-weight: 600 !important; margin: 20px 0 5px !important;
+  }
+  h4 {
+    font-weight: 600 !important; margin: 20px 0 5px !important;
+  }
+  h1 {
+    font-size: 22px !important;
+  }
+  h2 {
+    font-size: 18px !important;
+  }
+  h3 {
+    font-size: 16px !important;
+  }
+  .container {
+    width: 100% !important;
+  }
+  .content {
+    padding: 10px !important;
+  }
+  .content-wrapper {
+    padding: 10px !important;
+  }
+  .invoice {
+    width: 100% !important;
+  }
+  .nombre {
+  font-size: 24px;
+  color: #ff7d33 !important;
+  }
+
+}
+</style>
+</head>
+
+<body style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; -webkit-font-smoothing: antialiased; -webkit-text-size-adjust: none; width: 100% !important; height: 100%; line-height: 1.6; background: #f6f6f6; margin: 0; padding: 0;">
+
+<table class="body-wrap" style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; width: 100%; background: #f6f6f6; margin: 0; padding: 0;">
+    <tr style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; margin: 0; padding: 0;">
+        <td style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 0;" valign="top"></td>
+        <td class="container" width="600" style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; display: block !important; max-width: 600px !important; clear: both !important; margin: 0 auto; padding: 0;" valign="top">
+            <div class="content" style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; max-width: 600px; display: block; margin: 0 auto; padding: 20px;">
+                <table class="main" width="100%" cellpadding="0" cellspacing="0" style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; border-radius: 3px; background: #fff; margin: 0; padding: 0; border: 1px solid #e9e9e9;">
+                    <tr style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; margin: 0; padding: 0;">
+                        <td class="content-wrap" style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 20px;" valign="top">
+                            <table width="100%" cellpadding="0" cellspacing="0" style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; margin: 0; padding: 0;">
+
+                                <tr style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; margin: 0; padding: 0;">
+                                    <td class="content-block nombre" style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 0 0 20px;   font-size: 24px; color: #ff7d33 !important;" valign="top">
+                                        <img src="https://s3-sa-east-1.amazonaws.com/uploads-br.hipchat.com/97434/713107/dW1oe7t8Czge4SS/logo.png" width="305" height="114">
+                                    </td>
+                                </tr>
+
+
+                                <tr style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; margin: 0; padding: 0;">
+                                    <td class="content-block nombre" style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 0 0 20px;   font-size: 24px; color: #ff7d33 !important;" valign="top">
+                                        Hola $hombreUsuario
+                                    </td>
+                                </tr>
+                                <tr style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; margin: 0; padding: 0;">
+                                    <td class="content-block" style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 16px; vertical-align: top; margin: 0; padding: 0 0 20px;" valign="top">
+                                        Gracias por registrarte, pronto seras atendido por nuestro servicio de <strong>Atencion al cliente</strong>, mientras tanto te invitamos a conocer en detalle la perimetria y los servicios que ofrecidos por la <strong>ExpoFiss 2015.</strong>
+                                    </td>
+                                </tr>
+                                <tr style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; margin: 0; padding: 0;">
+                                    <td class="content-block nombre" style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 0 0 20px;   font-size: 24px; color: #ff7d33 !important;" valign="top">
+
+                                        <a href="http://expotachira.net/planimetria.html"> <img src="https://s3-sa-east-1.amazonaws.com/uploads-br.hipchat.com/97434/713107/00xpCdRCmvJdumm/ver.png" ></a>
+                                    </td>
+                                </tr>
+
+                            </table>
+                        </td>
+                    </tr>
+                </table>
+                <div class="footer" style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; width: 100%; clear: both; color: #999; margin: 0; padding: 20px;">
+                    <table width="100%" style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; margin: 0; padding: 0;">
+                        <tr style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; margin: 0; padding: 0;">
+                            <td class="aligncenter content-block" style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 12px; vertical-align: top; text-align: center; margin: 0; padding: 0 0 20px;" align="center" valign="top">Siguenos en <a href="http://twitter.com/laexpofiss" style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 12px; color: #999; text-decoration: underline; margin: 0; padding: 0;">@laexpotachira</a> en Twitter.</td>
+                        </tr>
+
+
+                    </table>
+                </div></div>
+        </td>
+        <td style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 0;" valign="top"></td>
+    </tr>
+</table>
+
+</body>
+</html>
+EOF;
 $body = $app->request->getBody();
 
 $app->get('/', function () {
@@ -55,16 +190,22 @@ $app->post('/savenew', function () use ($app) {
     }
 
     if ($status) {
-    	    $app->database->insert('preventaweb', [
-			'pre_emp' => $vars['empresa'],
-			'pre_con' => $vars['contacto'],
-			'pre_tel' => $vars['telefono'],
-			'pre_ema' => $vars['email'],
-			'pre_tip' => intval($vars['tipo']),
-			'pre_ruta' => intval($vars['pre_ruta']),
-			'pre_rutaid' => intval($vars['pre_rutaid'])
-			]);
-			$guardado = true;
+
+		$guardado = true;
+		$app->database->insert('preventaweb', [
+		'pre_emp' => $vars['empresa'],
+		'pre_con' => $vars['contacto'],
+		'pre_tel' => $vars['telefono'],
+		'pre_ema' => $vars['email'],
+		'pre_tip' => intval($vars['tipo']),
+		'pre_ruta' => intval($vars['pre_ruta']),
+		'pre_rutaid' => intval($vars['pre_rutaid'])
+		]);
+		$guardado = true;
+		$app->mg->sendMessage($app->mg_domain, array('from' => 'ventas@tuquiniela.net',
+		'to' => $vars['email'],
+		'subject' => "Preventa ExpoTachira 2015",
+		'html' => $app->email));
     }else{
     		$guardado = false;
     }
@@ -87,11 +228,11 @@ $app->post('/update/:tipo/:id', function ($tipo,$id) use ($app) {
     if ($tipo=="clipre") {
     	if ($app->database->has( "preventaweb" , ["pre_id" => $id ] )) {
     		$updatepre=array();
-		   	if (isset($vars['est'])) {
-		   		$updatepre['pre_est'] =  $vars['est'];
+		   	if (isset($vars['pre_est'])) {
+		   		$updatepre['pre_est'] =  $vars['pre_est'];
 		   	}
-		   	if (isset($vars['interes'])) {
-		   		$updatepre['pre_int'] =  $vars['interes'];
+		   	if (isset($vars['pre_int'])) {
+		   		$updatepre['pre_int'] =  $vars['pre_int'];
 		   	}
 		    $app->database->update('preventaweb', $updatepre , ["pre_id" => $id]);
 
@@ -101,6 +242,15 @@ $app->post('/update/:tipo/:id', function ($tipo,$id) use ($app) {
 
 			$respuesta->estatus = false;
 
+		}
+    }
+    if ($tipo=="firth") {
+    	if ($app->database->has( "preventaweb" , ["pre_id" => $id ] )) {
+		   	$updatepre['pre_est'] =  "1";
+		    $app->database->update('preventaweb', $updatepre , ["pre_id" => $id]);
+		    $respuesta->estatus = true;
+		}else{
+			$respuesta->estatus = false;
 		}
     }
 
