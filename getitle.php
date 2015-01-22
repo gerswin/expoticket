e2a686b896950754644e49d5c859ca4edcb9598e
gerswin <gerswin@parawebs.com>
2015-01-21 20:38:55 -0430

    fix

--- getitle.php
+++ getitle.php
@@ -1,16 +1,18 @@
 <?php
 
-function getTitle($Url){
-    $str = file_get_contents($Url);
-    if(strlen($str)>0){
-        preg_match("/\<title\>(.*)\<\/title\>/",$str,$title);
-        return $title[1];
-    }
-}
-//Example:
-$doc = new DOMDocument();
-@$doc->loadHTMLFile($_POST["url"]);
-$xpath = new DOMXPath($doc);
+$ch = curl_init();
+$timeout = 5;
+curl_setopt($ch, CURLOPT_URL, $_POST["url"]);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+$data = curl_exec($ch);
+curl_close($ch);
+
+$dom = new DomDocument();
+$dom->loadHtml($data);
+$xpath = new DOMXPath($dom);
+
 
 
   echo json_encode(array("title"=>$xpath->query('//title')->item(0)->nodeValue));
